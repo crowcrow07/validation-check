36d0c05bb6a6365ea45ec3d84ea5be257fff36ed
crowcrow07 <skathd3402@gmail.com>
2023-03-07 14:37:45 +0900

    과제 완료

--- src/script.js
+++ src/script.js
@@ -1,10 +1,34 @@
-// 동영상 강의에 나온 코드를 그대로 실습하세요
 // TODO : DOM으로부터 필요한 엘리먼트를 불러오세요.
 
+let elInputUserName = document.querySelector("#username");
+let elInputPassword = document.querySelector("#password");
+let elInputPasswordRetype = document.querySelector("#password-retype");
+let elFailureMessage = document.querySelector(".failure-message");
+let elSuccessMessage = document.querySelector(".success-message");
+let elMismatchMessage = document.querySelector(".mismatch-message");
+
+elInputUserName.onkeyup = function () {
+  if (isMoreThan4Length(elInputUserName.value)) {
+    elSuccessMessage.classList.remove("hide");
+    elFailureMessage.classList.add("hide");
+  } else {
+    elSuccessMessage.classList.add("hide");
+    elFailureMessage.classList.remove("hide");
+  }
+};
+
+elInputPasswordRetype.onkeyup = function () {
+  if (isMatch(elInputPassword.value, elInputPasswordRetype.value)) {
+    elMismatchMessage.classList.add("hide");
+  } else {
+    elMismatchMessage.classList.remove("hide");
+  }
+};
+
 function isMoreThan4Length(value) {
-  // TODO : 동영상 강의를 보고 이 함수를 완성하세요.
+  return value.length >= 4;
 }
 
-function isMatch (password1, password2) {
-  // TODO : 동영상 강의를 보고 이 함수를 완성하세요.
+function isMatch(password1, password2) {
+  return password1 === password2;
 }
